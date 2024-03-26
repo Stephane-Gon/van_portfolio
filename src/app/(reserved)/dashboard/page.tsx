727a3c648a1aae6fe2f501af20d53e91a08df0ef
Stephane-Gon <stephane_work@outlook.com>
2024-03-26 20:13:05 +0000

    chore: updates

--- src/app/(reserved)/dashboard/page.tsx
+++ src/app/(reserved)/dashboard/page.tsx
@@ -1,20 +1,8 @@
-import { supabaseAdmin } from "@/lib/supabase";
-import { getServerSession } from "next-auth";
 
 const Dashboard = async () => {
-  const { data: tools, error } = await supabaseAdmin.from('tools').select()
-  const session = await getServerSession()
-  console.log("🚀 ~ Dashboard ~ session:", session)
-  console.log("🚀 ~ Page ~ error:", error)
-  console.log("🚀 ~ Page ~ tools:", tools)
-
   // TODO - Depois posso remover esta height
   return (
-    <ul className="h-[110%]">
-      {tools?.map((tool: any) => (
-        <li key={`tool-${tool.id}`}>{tool.name}</li>
-      ))}
-    </ul>
+    <div className="h-[110%]">DASHBORD</div>
   )
 }
 
--- src/app/(reserved)/tools/page.tsx
+++ src/app/(reserved)/tools/page.tsx
@@ -1,7 +1,20 @@
+import { supabaseAdmin } from "@/lib/supabase";
+import { getServerSession } from "next-auth";
 
 const Tools = async () => {
+  const { data: tools, error } = await supabaseAdmin.from('tools').select()
+  const session = await getServerSession()
+  console.log("🚀 ~ Dashboard ~ session:", session)
+  console.log("🚀 ~ Page ~ error:", error)
+  console.log("🚀 ~ Page ~ tools:", tools)
+
+  // TODO - Começar a adicionar uma componente de listagem das tools
   return (
-    <p>Tools</p>
+    <ul>
+      {tools?.map((tool: any) => (
+        <li key={`tool-${tool.id}`}>{tool.name}</li>
+      ))}
+    </ul>
   )
 }
 
