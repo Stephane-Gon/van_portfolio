72014acf8e14bee134cde76fdc8218e352f7a1a6
Stephane-Gon <stephane_work@outlook.com>
2024-03-29 12:11:24 +0000

    feat: added eslint git-hooks and prettier

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+import WithBundleAnalyzer from '@next/bundle-analyzer';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+const withBundleAnalyzer = WithBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+export default withBundleAnalyzer(nextConfig)
