39244313fee2b31b51ef67e89dd0411609224dc3
Stephane-Gon <stephane_work@outlook.com>
2024-03-31 18:56:24 +0100

    chore: added generic component tab toggler!

--- src/design-system/organism/Tabs/index.tsx
+++ src/design-system/organism/Tabs/index.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import { useEffect } from 'react';
 import type { TOGGLE_TABS } from '@/constants';
 
 type TabsProps = {
@@ -8,18 +8,48 @@ type TabsProps = {
 };
 
 const Tabs = ({ activeTab, setActiveTab }: TabsProps) => {
-  // TODO - Adicionar uma animação na barra
+  useEffect(() => {
+    const animateTabBorder = async (tabId: string, fromTranslate: string, toTranslate: string) => {
+      const { gsap } = await import('gsap');
+      gsap.fromTo(
+        tabId,
+        {
+          duration: 0.3,
+          translateX: fromTranslate,
+        },
+        {
+          duration: 0.3,
+          translateX: toTranslate,
+        },
+      );
+    };
+
+    if (activeTab === 'list') {
+      animateTabBorder('#border-list', '100%', '0%');
+      animateTabBorder('#border-detail', '0%', '100%');
+    } else {
+      animateTabBorder('#border-list', '0%', '-100%');
+      animateTabBorder('#border-detail', '-100%', '0%');
+    }
+  }, [activeTab]);
+
   return (
-    <div className=' flex items-center gap-2'>
+    <div className='flex items-center'>
       <span
         onClick={() => setActiveTab('list')}
-        className={`${activeTab === 'list' ? 'border-b-2 border-b-primary text-primary' : 'text-text'} flex min-w-[100px] cursor-pointer justify-center px-4 text-lg `}>
-        LIST
+        className={`flex min-w-[100px] cursor-pointer flex-col items-center pb-1 2sm:min-w-[150px]`}>
+        <p className={`${activeTab === 'list' ? 'text-primary' : 'text-text'} text-base`}>LIST</p>
+        <span
+          id='border-list'
+          className={`${activeTab === 'list' ? 'w-full' : 'w-0'} h-[2px] rounded-full bg-gradient-to-r from-secondary via-tertiary to-primary`}></span>
       </span>
       <span
         onClick={() => setActiveTab('detail')}
-        className={`${activeTab === 'detail' ? 'border-b-2 border-b-primary text-primary' : 'text-text'} flex min-w-[100px] cursor-pointer justify-center px-4 text-lg`}>
-        DETAIL
+        className={`flex min-w-[100px] cursor-pointer flex-col items-center pb-1 2sm:min-w-[150px]`}>
+        <p className={`${activeTab === 'detail' ? 'text-primary' : 'text-text'} text-base`}>DETAIL</p>
+        <span
+          id='border-detail'
+          className={`${activeTab === 'detail' ? 'w-full' : 'w-0'} h-[2px] rounded-full bg-gradient-to-r from-secondary via-tertiary to-primary`}></span>
       </span>
     </div>
   );
