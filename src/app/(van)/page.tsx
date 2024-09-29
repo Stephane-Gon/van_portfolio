5acf6289b720a64ff173ca9ed934fe1448d58656
Stephane-Gon <stephane_work@outlook.com>
2024-09-29 18:40:44 +0100

    feat: added serviceworkers

--- src/app/(van)/page.tsx
+++ src/app/(van)/page.tsx
@@ -3,12 +3,17 @@ import { useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
 import dynamic from 'next/dynamic';
 import { useThreeStore } from '@/features/three/store/useThree';
+import useServiceWorker from '@/hooks/useServiceWorker';
 
 const Scene = dynamic(() => import('@/features/three/components/Scene'), { ssr: false });
+
+// TODO - Tentar passar os models para um serviceworker
 
 const Home = () => {
   const searchParams = useSearchParams();
   const setDebugMode = useThreeStore(state => state.setDebugMode);
+
+  useServiceWorker();
 
   useEffect(() => {
     if (searchParams.get('debug') === 'true') {
