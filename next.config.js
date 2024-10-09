0b62baf0d89b373900d4703fc9dcb1095d1369b4
Stephane-Gon <stephane_work@outlook.com>
2024-10-09 22:01:00 +0100

    feat: added glsl file loading capability

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@ import WithBundleAnalyzer from '@next/bundle-analyzer';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag|ps)$/,
+      exclude: /node_modules/,
+      use: ['raw-loader', 'glslify-loader'],
+    });
+
+    return config;
+  },
   images: {
     remotePatterns: [
       {
