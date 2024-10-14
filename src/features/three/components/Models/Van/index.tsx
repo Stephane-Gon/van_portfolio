27c24477abb3d9604ce50f03cd431f4118b616c4
Stephane-Gon <stephane_work@outlook.com>
2024-10-14 22:07:47 +0100

    chore: created van points component

--- src/features/three/components/Models/Van/index.tsx
+++ src/features/three/components/Models/Van/index.tsx
@@ -1,44 +1,16 @@
 'use client';
 
 import { useGLTF, Clone } from '@react-three/drei';
-import { useAppStore } from '@/features/app/store';
-import { useControls } from 'leva';
-import * as THREE from 'three';
-import Point from '../../Point';
-import { Themes } from '@/features/app/types';
+import Points from './Points';
 
 export default function Tv() {
   const van = useGLTF('./models/van.glb');
-  const toggleTheme = useAppStore(state => state.toggleTheme);
-  const theme = useAppStore(state => state.theme);
-
-  // TODO - Tentar comprimir os glb files
-
-  const { switcherPoint } = useControls('van', {
-    switcherPoint: {
-      value: { x: -1.4, y: 1.4, z: 2.3 },
-      step: 0.01,
-    },
-  });
-
-  const handleSwitchTheme = () => {
-    if (theme === Themes.light) {
-      toggleTheme(Themes.dark);
-    } else if (theme === Themes.dark) {
-      toggleTheme(Themes.light);
-    }
-  };
+  // TODO - Resize da van dependendo dos viewports;
 
   return (
     <group>
       <Clone object={van.scene} position={[0.5, 0.2, 2]} scale={0.6} rotation={[0, Math.PI * 0.65, 0]} />
-      <Point
-        position={new THREE.Vector3(switcherPoint.x, switcherPoint.y, switcherPoint.z)}
-        label='1'
-        description='Toggle the environment theme!'
-        sizes='small'
-        onClick={handleSwitchTheme}
-      />
+      <Points />
     </group>
   );
 }
