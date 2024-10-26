1d81260f0c0eb706924c59fb387a61297981ac1b
Stephane-Gon <stephane_work@outlook.com>
2024-10-26 14:54:49 +0100

    chore: added new design system components

---
+++ src/design-system/atoms/MainBurgerIcon/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const MainBurgerIcon = () => {
+  return (
+    <div className='flex h-5 items-center justify-center gap-1 overflow-hidden'>
+      <div className='flex translate-y-[8px] flex-col gap-[2px] duration-300 group-hover:translate-y-[-8px]'>
+        <div className='h-5 w-[2px] rounded-md bg-accent opacity-45'></div>
+        <div className='h-1 w-[2px] rounded-md bg-accent opacity-45'></div>
+        <div className='h-5 w-[2px] rounded-md bg-accent opacity-85'></div>
+      </div>
+      <div className='h-4 w-[2px] rounded-md bg-accent opacity-45'></div>
+      <div className='flex translate-y-[8px] flex-col gap-[2px] duration-300 group-hover:translate-y-[-8px]'>
+        <div className='h-5 w-[2px] rounded-md bg-accent opacity-45'></div>
+        <div className='h-1 w-[2px] rounded-md bg-accent opacity-45'></div>
+        <div className='h-5 w-[2px] rounded-md bg-accent opacity-85'></div>
+      </div>
+    </div>
+  );
+};
+
+export default MainBurgerIcon;
