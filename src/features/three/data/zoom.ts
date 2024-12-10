591cc5a76d9c8ed244486919149a1f5b8abc8fe7
Stephane-Gon <stephane_work@outlook.com>
2024-12-10 15:04:07 +0000

    chore: added zoom data file

---
+++ src/features/three/data/zoom.ts
@@ -0,0 +1,72 @@
+import type { ZoomData } from '../types';
+
+export const projectsZoomData: ZoomData = {
+  default: {
+    position: {
+      x: -0.67,
+      y: 1.27,
+      z: 2.25,
+    },
+    rotation: {
+      x: 0.11,
+      y: 0.47,
+      z: -0.05,
+    },
+    html: {
+      x: -4.07,
+      y: 2.83,
+      z: 0.4,
+    },
+  },
+  600: {
+    position: {
+      x: 0.2,
+      y: 1.305,
+      z: 2.35,
+    },
+    rotation: {
+      x: 0.11,
+      y: 0.47,
+      z: -1.62,
+    },
+    html: {
+      x: -4.06,
+      y: 2.83,
+      z: 0.4,
+    },
+  },
+  1000: {
+    position: {
+      x: -0.4,
+      y: 1.08,
+      z: 2.35,
+    },
+    rotation: {
+      x: 0.11,
+      y: 0.47,
+      z: -1.62,
+    },
+    html: {
+      x: -4.06,
+      y: 2.83,
+      z: 0.4,
+    },
+  },
+  2500: {
+    position: {
+      x: -0.67,
+      y: 1.27,
+      z: 2.25,
+    },
+    rotation: {
+      x: 0.11,
+      y: 0.47,
+      z: -0.05,
+    },
+    html: {
+      x: -4.06,
+      y: 2.83,
+      z: 0.4,
+    },
+  },
+};
